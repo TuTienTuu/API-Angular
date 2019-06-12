d3719e40889653d1a4f39db0c5b2c6eea31167e9
TuuTuTien <tutientuu140596@gmail.com>
2019-06-12 09:54:54 +0700

    add entity framwork code first for data

---
+++ Web.Data/Infrastructure/DbFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Data.Infrastructure
+{
+    internal class DbFactory : Disposable, IDbFactory
+    {
+        private WebDbContext dbContext;
+
+        public WebDbContext Init()
+        {
+            return dbContext ?? (dbContext = new WebDbContext());
+        }
+
+        protected override void DisposeCore()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+        }
+    }
+}
